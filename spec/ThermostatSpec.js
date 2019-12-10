6d3265c0733b2b710c51f9ce7088fcbcce3c63f7
mona lisa <sibaj@hotmail.co.uk>
2019-12-10 12:07:16 +0000

    added up function and down function

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,6 +1,22 @@
-"use strict"
+"use strict";
 
 describe("Thermostat", function() {
-  it("does nothing", function() {
-    expect().nothing();
-    
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+    it("starts at 20 degrees", function() {
+      expect(thermostat.getCurrenttemperature()).toEqual(20);
+    });
+    it("increases in temperature with up()", function() {
+      thermostat.up();
+      expect(Thermostat.getCurrentTemperature()).toEqual(21);
+    });
+    it("decreases in temperature with down button", function () {
+      thermostat.down();
+      expect(thermostat.getCurrenttemperature()).toEqual(19);
+
+    });
+  });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,4 +1,25 @@
-"use strict"
+"use strict";
+//the function below declares thermostat function and the properties
+// are that it returns 20. this creates an instance of
+//thermostat object by running new Thermostat() with a property of 20.
+function Thermostat() {
 
-class Thermostat {
-}
+  this.temperature = 20;
+
+};
+
+  Thermostat.prototype.getCurrentTemperature = function () {
+    return this.temperature; };
+//The prototype keyword acts as a storage area for Thermostat and
+// binds the methodName 'getCurrenttemperature' to that function
+
+// example code on binding a function =
+//ClassName.prototype.methodName = function () {
+
+    Thermostat.prototype.up = function () {
+      this.temperature += 1;
+    };
+
+    Thermostat.prototype.down = function () {
+      this.temperature -= 1;
+    };
