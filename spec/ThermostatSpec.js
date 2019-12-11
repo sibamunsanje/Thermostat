73c5fb9b5356bcba4198884a74445cbc6021e503
mona lisa <sibaj@hotmail.co.uk>
2019-12-11 11:43:24 +0000

    can turn power saving mode on and off

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -19,13 +19,23 @@ describe("Thermostat", function() {
       expect(thermostat.getCurrentTemperature()).toEqual(19);
 
     });
-    it("has a minimum temperature of 10 degrees"), function() {
+    it("has a minimum temperature of 10 degrees", function() {
       for (var i = 0; i < 11; i++) {
         thermostat.down();
       }
       expect(thermostat.getCurrentTemperature()).toEqual(10);
-    }
-    it("has a power saving mode"), function () {
+    });
+    it("has a power saving mode", function () {
       expect(thermostat.isPowerSavingModeOn()).toBe(true);
-    };
+    });
+    it("can turn off power saving mode", function() {
+      thermostat.turnOffPowerSavingMode();
+      expect(thermostat.isPowerSavingModeOn()).toBe(false);
+    });
+    it("can turn power saving mode back on", function() {
+      thermostat.turnOffPowerSavingMode();
+      expect(thermostat.isPowerSavingModeOn()).toBe(false);
+      thermostat.turnOnPowerSavingMode();
+      expect(thermostat.isPowerSavingModeOn()).toBe(true);
+});
   });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,45 +1,51 @@
 "use strict";
-//the function below declares thermostat function and the properties
+// the function below declares thermostat function and the properties
 // are that it returns 20. this creates an instance of
-//thermostat object by running new Thermostat() with a property of 20.
+// thermostat object by running new Thermostat() with a property of 20.
 function Thermostat() {
   this.MINIMUMTEMP = 10;
-  this.MAXPOWERSAVINGTEMP = 25
+  this.POWERSAVINGMODE = true
   this.temperature = 20;
 
 };
 
   Thermostat.prototype.getCurrentTemperature = function () {
-    return this.temperature; };
+    return this.temperature;
+  };
 
-  Thermostat.prototype.isMinimumTemperarture = function () {
+  Thermostat.prototype.isMinimumTemperature = function () {
     return this.temperature === this.MINIMUMTEMP;
   };
 
   Thermostat.prototype.isPowerSavingModeOn = function() {
-    return this.temperature > this.MAXPOWERSAVINGTEMP;
+    return this.POWERSAVINGMODE === true;
   };
-  // Is is a function intended to return a boolean value.
-  // === means a value is only strictly equal to itself.
-//The prototype keyword acts as a storage area for Thermostat and
+  Thermostat.prototype.turnOffPowerSavingMode = function() {
+    return this.POWERSAVINGMODE = false;
+  };
+  Thermostat.prototype.turnOnPowerSavingMode = function() {
+  this.POWERSAVINGMODE = true;
+  };
+//   Is is a function intended to return a boolean value.
+//   === means a value is only strictly equal to itself.
+// The prototype keyword acts as a storage area for Thermostat and
 // binds the methodName 'getCurrenttemperature' to that function
-
+//
 // example code on binding a function =
-//ClassName.prototype.methodName = function () {
+// ClassName.prototype.methodName = function () {
 
-    Thermostat.prototype.up = function () {
+  Thermostat.prototype.up = function() {
       this.temperature += 1;
     };
 
-    Thermostat.prototype.down = function () {
-      if (this.isMinimumTemperarture()) {
+  Thermostat.prototype.down = function() {
+      if (this.isMinimumTemperature()) {
         return;
       }
         this.temperature -= 1;
-    }
-    Thermostat.prototype.powersavingmode = function () {
-      if (this.isPowerSavingModeOn()) {
-        return;
-      }
-        this.MAXPOWERSAVINGTEMP; 
-    }
+    };
+  // Thermostat.prototype.isPowerSavingModeOn = function () {
+  //     if (this.isPowerSavingModeOn()) {
+  //       return this.POWERSAVINGMODE;
+  //   };
+  // };
